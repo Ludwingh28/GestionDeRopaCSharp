6324e171d4555289adcb7cfec96ee4e7f6096c94
Ludwing Herrera Justiniano <ludwingh2807@outlook.com>
2025-05-03 03:53:29 -0400

    Creacion del Loging, Por la tarde voy a comenzar a ver lo del dashboard y lo de los iconos del logign y lo de las letras que de momento no me desagradan.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace GestionDeRopaCSharp
         {
             InitializeComponent();
         }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e) 
+        {
+            WindowState = WindowState.Minimized;
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
